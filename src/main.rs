016b6ccbd1478e41e52539aa475eb31ab5f6cc06
志宇 <hello@evanlinjin.me>
2023-07-16 00:34:38 +0800

    Implement more network stats
    Also:
    * Move `Echology` into new file
    * Implement tls
    * Config via ENVs

--- src/main.rs
+++ src/main.rs
@@ -1,167 +1,30 @@
-use std::collections::BTreeMap;
-
-use async_std::sync::Arc;
-use async_std::sync::RwLock;
-
-use async_std::task::JoinHandle;
 use bdk_bitcoind_rpc::bitcoincore_rpc::jsonrpc::serde_json::Value;
 use bdk_bitcoind_rpc::bitcoincore_rpc::RawTx;
 use bdk_chain::bitcoin::Address;
 use bdk_chain::bitcoin::Amount;
-use bdk_chain::local_chain::LocalChain;
 use bitcoind::anyhow::anyhow;
 use bitcoind::bitcoincore_rpc::RpcApi;
+use echo::*;
 use tide::prelude::*;
 use tide::Request;
 use tide::Response;
 use tide::StatusCode;
-use wally::CsSolutionRequest;
-use wally::CsSolutionResponse;
-use wally::Scenario;
-use wally::Wally;
+use tide_rustls::TlsListener;
+use wally::*;
 
+mod echo;
 mod wally;
-
-const BLOCK_TIME_SECONDS: u64 = 30;
-
-type EchologyJoinHandles = [JoinHandle<tide::Result<()>>; 3];
-
-#[derive(Clone)]
-struct Echology {
-    pub bitcoind: Arc<bitcoind::BitcoinD>,
-    pub chain: Arc<RwLock<LocalChain>>,
-    pub wallets: Arc<RwLock<BTreeMap<String, Arc<RwLock<Wally>>>>>,
-}
-
-impl Echology {
-    fn new(d: bitcoind::BitcoinD) -> tide::Result<(Self, EchologyJoinHandles)> {
-        tide::log::start();
-        // local chain
-        let genesis_hash = d.client.get_block_hash(0)?;
-        let local_chain = LocalChain::from_blocks([(0, genesis_hash)].into());
-        let local_chain_tip = local_chain.tip().expect("must have tip");
-
-        let url = format!(
-            "{}:{}",
-            d.params.rpc_socket.ip(),
-            d.params.rpc_socket.port()
-        );
-        let auth =
-            bdk_bitcoind_rpc::bitcoincore_rpc::Auth::CookieFile(d.params.cookie_file.clone());
-        let (tx, rx) = std::sync::mpsc::sync_channel::<bdk_bitcoind_rpc::BitcoindRpcUpdate>(10);
-
-        let echology = Self {
-            bitcoind: Arc::new(d),
-            chain: Arc::new(RwLock::new(local_chain)),
-            wallets: Default::default(),
-        };
-
-        let emitter_jh = async_std::task::spawn_blocking(move || -> tide::Result<()> {
-            let client = bdk_bitcoind_rpc::bitcoincore_rpc::Client::new(&url, auth)?;
-            let mut emitter =
-                bdk_bitcoind_rpc::BitcoindRpcEmitter::new(&client, 0, Some(local_chain_tip));
-            loop {
-                match emitter.next_update() {
-                    Ok(update) => {
-                        let is_mempool = update.is_mempool();
-                        tide::log::info!(
-                            "[emitter] found update: {}",
-                            match &update {
-                                bdk_bitcoind_rpc::BitcoindRpcUpdate::Block { cp, .. } => {
-                                    format!("block: height={}, hash={}", cp.height(), cp.hash())
-                                }
-                                bdk_bitcoind_rpc::BitcoindRpcUpdate::Mempool { cp, txs } => {
-                                    format!(
-                                        "mempool: height={}, tx_count={}",
-                                        cp.height(),
-                                        txs.len()
-                                    )
-                                }
-                            }
-                        );
-                        tx.send(update)?;
-                        if is_mempool {
-                            std::thread::sleep(std::time::Duration::from_secs(5));
-                        }
-                    }
-                    Err(err) => {
-                        tide::log::error!("[emitter] {}: sleeping for 5 secs", err);
-                        std::thread::sleep(std::time::Duration::from_secs(5));
-                        continue;
-                    }
-                };
-            }
-        });
-
-        let absorber_jh = {
-            let echology = echology.clone();
-            async_std::task::spawn(async move {
-                for update in rx {
-                    let local_update = update
-                        .into_update::<(), _, _>(bdk_bitcoind_rpc::confirmation_height_anchor);
-
-                    echology
-                        .chain
-                        .write()
-                        .await
-                        .update(local_update.tip, true)?;
-
-                    let wallets = echology.wallets.read().await;
-                    for wallet in wallets.values() {
-                        let mut wallet = wallet.write().await;
-                        let _ = wallet
-                            .indexed_tx_graph
-                            .apply_update(local_update.graph.clone());
-                    }
-                }
-
-                return tide::Result::Ok(());
-            })
-        };
-
-        let miner_jh = {
-            let echology = echology.clone();
-            async_std::task::spawn_blocking(move || -> tide::Result<()> {
-                let address = echology.bitcoind.client.get_new_address(None, None)?;
-                let hashes = echology
-                    .bitcoind
-                    .client
-                    .generate_to_address(420, &address)?;
-                tide::log::info!(
-                    "[miner] generated to height={}, tip_hash={}",
-                    hashes.len(),
-                    hashes.last().unwrap()
-                );
-
-                loop {
-                    std::thread::sleep(std::time::Duration::from_secs(BLOCK_TIME_SECONDS));
-                    let hashes = echology.bitcoind.client.generate_to_address(1, &address)?;
-                    if let Some(hash) = hashes.last() {
-                        tide::log::info!("[miner] found block: {}", hash);
-                    }
-                }
-            })
-        };
-
-        Ok((echology, [emitter_jh, absorber_jh, miner_jh]))
-    }
-
-    pub async fn get_or_create_wallet(&self, phrase: &str) -> tide::Result<Arc<RwLock<Wally>>> {
-        let wallet = match self.wallets.write().await.entry(phrase.to_string()) {
-            std::collections::btree_map::Entry::Vacant(e) => {
-                let wallet = Arc::new(RwLock::new(Wally::new(phrase)?));
-                Arc::clone(&*e.insert(wallet))
-            }
-            std::collections::btree_map::Entry::Occupied(e) => Arc::clone(e.get()),
-        };
-        Ok(wallet)
-    }
-}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    // std::env::var_os("");
-    let (state, join_handles) = Echology::new(bitcoind::BitcoinD::from_downloaded()?)?;
+    let env_bitcoind = std::env::var("ECHO_BITCOIND")?;
+    let env_blocktime = std::env::var("ECHO_BLOCKTIME")?.parse::<u64>()?;
+    let env_bind = std::env::var("ECHO_BIND")?;
+    let env_tls_cert = std::env::var_os("ECHO_TLS_CERT");
+    let env_tls_key = std::env::var_os("ECHO_TLS_KEY");
+
+    let (state, join_handles) =
+        Echology::new(bitcoind::BitcoinD::new(env_bitcoind)?, env_blocktime)?;
 
     let mut app = tide::with_state(state);
     app.with(tide::utils::After(|mut resp: Response| async move {
@@ -183,7 +46,20 @@ async fn main() -> tide::Result<()> {
         .post(wallet_new_spend_scenario);
     app.at("/wallet/:alias/new_solution")
         .post(wallet_new_solution);
-    app.listen("127.0.0.1:8080").await?;
+
+    match (env_tls_cert, env_tls_key) {
+        (Some(tls_cert), Some(tls_key)) => {
+            app.listen(
+                TlsListener::build()
+                    .addrs(env_bind)
+                    .cert(tls_cert)
+                    .key(tls_key),
+            )
+            .await?
+        }
+        (None, None) => app.listen(env_bind).await?,
+        _ => panic!("must provide both tls cert and key"),
+    };
 
     for handle in join_handles {
         handle.await?;
@@ -193,9 +69,8 @@ async fn main() -> tide::Result<()> {
 }
 
 async fn network_stats(req: Request<Echology>) -> tide::Result {
-    let chain = req.state().chain.read().await;
-    let height = chain.tip().map_or(0, |cp| cp.height());
-    Ok(json!({ "height": height }).into())
+    let stats = req.state().stats.read().await.clone();
+    Ok(json!(stats).into())
 }
 
 async fn network_broadcast(req: Request<Echology>) -> tide::Result {
