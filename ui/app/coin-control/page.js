859cf2c88dcefacfc97dd18aa3e436f778157696
Echoooo <echo@findrecruiter.com>
2023-07-15 11:35:25 +0700

    feat: sync all SelectionDropdown

--- ui/app/coin-control/page.js
+++ ui/app/coin-control/page.js
@@ -1,36 +1,93 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import SelectionDropdown from "@components/SelectionDropdown";
 import Table from "@app/coin-control/components/Table";
+import { GET } from "@utils/request";
+import { convertSelectedValue } from "@app/coin-control/components/converter";
+import Cookies from "js-cookie";
 
 const CoinControl = () => {
-  const [globalSelect, setGlobalSelect] = useState(undefined);
+  const [selectAllAs, setSelectAllTo] = useState(undefined);
+  const [selectedCoins, setSelectedCoins] = useState([]);
+  const [myCoins, setMyCoins] = useState([]);
 
-  const handleGlobalSelect = useCallback((e) => {
-    setGlobalSelect(e.target.value);
+  const alias = Cookies.get("alias");
+
+  useEffect(() => {
+    GET(`${process.env.SERVER_HOST}/wallet/${alias}/coins`).then((result) => {
+      const data = result.coins;
+      const coinsWithSelection = data.map((coin) => {
+        return { ...coin, must_select: undefined };
+      });
+      setMyCoins(coinsWithSelection);
+    });
   }, []);
 
-  const handleClearGlobalSelect = useCallback(() => {
-    setGlobalSelect(undefined);
-  }, []);
+  useEffect(() => {
+    const filteredCoins = myCoins.filter(
+      (coin) => coin.must_select !== undefined,
+    );
+    setSelectedCoins(filteredCoins);
+  }, [myCoins]);
+
+  const handleSelectAllAs = useCallback(
+    (e) => {
+      const selectedValue = convertSelectedValue(e.target.value);
+      const newCoins = myCoins.map((coin) => {
+        return { ...coin, must_select: selectedValue };
+      });
+      setMyCoins(newCoins);
+      setSelectAllTo(selectedValue);
+    },
+    [myCoins],
+  );
+
+  const handleClearAllSelection = useCallback(() => {
+    const deselectedCoins = myCoins.map((coin) => {
+      return { ...coin, must_select: undefined };
+    });
+    setMyCoins(deselectedCoins);
+    setSelectAllTo(undefined);
+  }, [myCoins]);
+
+  const hasAtLeastOneSelected = useMemo(() => {
+    const result = myCoins.map((coin) => {
+      const { must_select } = coin;
+      if (must_select !== undefined && must_select !== null) {
+        return 1;
+      }
+      return 0;
+    });
+    return result.find((select) => select === 1) || 0;
+  }, [myCoins]);
+
   return (
     <div className="page_padding flex flex-col gap-8">
       <div className="flex justify-between w-full items-end ">
         <div className="page_title">Coin Control</div>
         <div className="flex flex gap-10">
-          <button className="main_button" onClick={handleClearGlobalSelect}>
+          <button
+            className="main_button disabled:cursor-not-allowed disabled:text-gray-500"
+            onClick={handleClearAllSelection}
+            disabled={hasAtLeastOneSelected === 0}
+          >
             Clear Selection
           </button>
           <div className="flex gap-3 items-center">
             <span>Select all as</span>
             <SelectionDropdown
-              onGlobalSelect={handleGlobalSelect}
-              value={globalSelect}
+              onGlobalSelect={handleSelectAllAs}
+              selected={selectAllAs}
             />
           </div>
         </div>
       </div>
-      <Table globalSelect={globalSelect} />
+      <Table
+        selectAllAs={selectAllAs}
+        coins={myCoins}
+        setCoins={setMyCoins}
+        selectedCoins={selectedCoins}
+      />
     </div>
   );
 };
--- ui/components/SelectionDropdown.js
+++ ui/components/SelectionDropdown.js
@@ -1,20 +1,23 @@
-const SELECT_VALUE_MUST = "Must Select";
-const SELECT_VALUE_CAN = "Can Select";
-const SELECT_VALUE_DONT = "Don't Select";
-
-const SelectionDropdown = ({ onGlobalSelect, selected }) => {
+const SelectionDropdown = ({ onGlobalSelect, selected, onChange }) => {
+  console.log("selected", selected);
   return (
     <select
       className="select select-bordered rounded-none main_background rounded-none whitespace-nowrap border border-black"
-      onChange={onGlobalSelect}
+      onChange={onGlobalSelect || onChange}
     >
       <option disabled selected={selected === undefined}>
         please select
       </option>
-      <option selected={selected === SELECT_VALUE_MUST}>Must Select</option>
-      <option selected={selected === SELECT_VALUE_CAN}>Can Select</option>
+      <option selected={selected === true} value={1}>
+        Must Select
+      </option>
+      <option selected={selected === false} value={0}>
+        Can Select
+      </option>
       {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <option selected={selected === SELECT_VALUE_DONT}>Don't Select</option>
+      <option selected={selected === null} value={-1}>
+        Don't Select
+      </option>
     </select>
   );
 };
