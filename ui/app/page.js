a8da86f97faa89aff8854086aeed7e7536726bd7
志宇 <hello@evanlinjin.me>
2023-07-16 15:41:50 +0800

    UI works as static

--- ui/app/page.js
+++ ui/app/page.js
@@ -27,7 +27,7 @@ const WelcomePage = () => {
       window.my_modal_4.showModal();
     }
     setCookie("alias", alias);
-    GET(`http://localhost:8080/api/wallet/${alias}/address`).then((result) =>
+    GET(`api/wallet/${alias}/address`).then((result) =>
       setCookie("address", result.address),
     );
   };
