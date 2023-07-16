2afe8cad3884743d7a20bad17888cedf0e75b614
Echoooo <echo@findrecruiter.com>
2023-07-16 14:14:13 +0700

    feat: update solutions layout

--- ui/components/RecipientsTable.js
+++ ui/components/RecipientsTable.js
@@ -14,49 +14,52 @@ const RecipientsTable = ({
       <table className="main_table">
         <thead>
           <tr>
-            <th></th>
+            <th>-</th>
             <TableHead label="Address:" />
             <TableHead label="Amount:" />
-            <th></th>
+            <th>-</th>
           </tr>
         </thead>
         <tbody>
-          {recipients.length > 0 && recipients.map((recipient, index) => (
-            <tr className="hover" key={`recipient-${index}`}>
-              <th>{index}</th>
-              <td>
-                <input
-                  key={`address-${index}`}
-                  value={recipient.address}
-                  id={index}
-                  type="text"
-                  className="input_field w-full"
-                  onChange={onChangeRecipientAddress}
-                  disabled={isDone === true}
-                />
-              </td>
-              <td>
-                <input
-                  id={index}
-                  key={`amount-${index}`}
-                  value={recipient.amount}
-                  type="number"
-                  className="input_field w-full"
-                  onChange={onChangeRecipientAmount}
-                  disabled={isDone === true}
-                />
-              </td>
-              <td>
-                <button
-                  disabled={isDone === true}
-                  onClick={onDeleteRecipient}
-                  className="flex items-center justify-center hover:bg-gray-500 rounded-full hover:text-white disabled:text-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed"
-                >
-                  <RiCloseFill id={index} />
-                </button>
-              </td>
-            </tr>
-          ))}
+          {recipients.length > 0 &&
+            recipients.map((recipient, index) => (
+              <tr className="hover" key={`recipient-${index}`}>
+                <th>{index}</th>
+                <td>
+                  <input
+                    key={`address-${index}`}
+                    value={recipient.address}
+                    id={index}
+                    type="text"
+                    className="input_field w-full"
+                    onChange={onChangeRecipientAddress}
+                    disabled={isDone === true}
+                  />
+                </td>
+                <td>
+                  <input
+                    id={index}
+                    key={`amount-${index}`}
+                    value={recipient.amount}
+                    type="number"
+                    className="input_field w-full"
+                    onChange={onChangeRecipientAmount}
+                    disabled={isDone === true}
+                  />
+                </td>
+                <td>
+                  <button
+                    disabled={isDone === true}
+                    onClick={onDeleteRecipient}
+                    className="flex items-center justify-center hover:bg-gray-500 rounded-full hover:text-white disabled:text-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed"
+                  >
+                    <div className="grid place-items-center h-full w-full ">
+                      <RiCloseFill id={index} />
+                    </div>
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
