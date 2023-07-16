0f87dbb4d54c59ba11c77a23b908c526d9ae1ba2
Echoooo <echo@findrecruiter.com>
2023-07-16 13:18:49 +0700

    fix: resolve error

--- ui/components/SolutionTable.js
+++ ui/components/SolutionTable.js
@@ -16,79 +16,99 @@ const SolutionTable = ({ solutions }) => {
   ];
 
   const handleBroadcast = useCallback((hex) => {
-    POST(`http://localhost:8080/api/network/broadcast?tx=${hex}`).then((r) =>
-      console.log("r", r),
+    POST(`http://localhost:8080/api/network/broadcast?tx=${hex}`).then(() =>
+      window.my_modal_2.showModal(),
     );
   }, []);
 
   return (
-    <table className="main_table">
-      <thead>
-        <tr>
-          <th></th>
-          {titles.map((title, index) => (
-            <TableHead key={index} label={title} />
-          ))}
-          <th></th>
-        </tr>
-      </thead>
-      {solutions.length === 0 ? (
-        "no solutions yet"
-      ) : (
-        <tbody>
-          {solutions.length > 0 &&
-            solutions.map((solution, index) => {
-              const { timestamp, txid, request, metrics, raw_tx } = solution;
-              const {
-                used_excess_strategy,
-                waste,
-                tx_size,
-                feerate_deviation,
-              } = metrics;
-              const time = `${new Date(timestamp * 1000).getHours()}:${new Date(
-                timestamp * 1000,
-              ).getMinutes()}`;
-              const txId = `${txid.substring(0, 6)}.....${txid.substring(
-                txid.length - 6,
-              )}`;
+    <>
+      <table className="main_table">
+        <thead>
+          <tr>
+            <th></th>
+            {titles.map((title, index) => (
+              <TableHead key={index} label={title} />
+            ))}
+            <th></th>
+          </tr>
+        </thead>
+        {solutions.length === 0 ? (
+          "no solutions yet"
+        ) : (
+          <tbody>
+            {solutions.length > 0 &&
+              solutions.map((solution, index) => {
+                const { timestamp, txid, request, raw_tx } = solution;
+                const time = `${new Date(
+                  timestamp * 1000,
+                ).getHours()}:${new Date(timestamp * 1000).getMinutes()}`;
+                const txId = `${txid.substring(0, 6)}.....${txid.substring(
+                  txid.length - 6,
+                )}`;
 
-              return (
-                <tr className="hover">
-                  <td>{index}</td>
-                  <td className="input_field">{time}</td>
-                  <td className="input_field">{txId}</td>
-                  {request.algorithm ? (
-                    <td className="input_field">{request.algorithm}</td>
-                  ) : (
-                    <td className="input_field">--</td>
-                  )}
-                  <td className="input_field capitalize">
-                    {used_excess_strategy.split("_").join(" ")}
-                  </td>
-                  <td className="input_field">{waste}</td>
-                  <td className="input_field">{feerate_deviation}</td>
-                  <td className="input_field">{tx_size}</td>
-                  <td className="flex gap-6 items-center h-full pt-2">
-                    <button
-                      onClick={() => handleBroadcast(raw_tx)}
-                      className="tooltip tooltip-bottom"
-                      data-tip="Broadcast"
-                    >
-                      <RiBroadcastLine fontSize={24} />
-                    </button>
-                    <button
-                      className="tooltip tooltip-bottom"
-                      data-tip="More Detail"
-                    >
-                      <AiOutlineExclamationCircle fontSize={24} />
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      )}
-    </table>
+                return (
+                  <tr className="hover">
+                    <td>{index}</td>
+                    <td className="input_field">{time}</td>
+                    <td className="input_field">{txId}</td>
+                    {request.algorithm ? (
+                      <td className="input_field capitalize">
+                        {request.algorithm.split("_").join(" ")}
+                      </td>
+                    ) : (
+                      <td className="input_field">--</td>
+                    )}
+                    <td className="input_field capitalize">
+                      {solution.metrics &&
+                        solution.metrics.used_excess_strategy
+                          .split("_")
+                          .join(" ")}
+                    </td>
+                    <td className="input_field">
+                      {solution.metrics && solution.metrics.waste}
+                    </td>
+                    <td className="input_field">
+                      {solution.metrics && solution.metrics.feerate_deviation}
+                    </td>
+                    <td className="input_field">
+                      {solution.metrics && solution.metrics.tx_size}
+                    </td>
+                    <td className="flex gap-6 items-center h-full pt-2">
+                      <button
+                        onClick={() => handleBroadcast(raw_tx)}
+                        className="tooltip tooltip-bottom"
+                        data-tip="Broadcast"
+                      >
+                        <RiBroadcastLine fontSize={24} />
+                      </button>
+                      <button
+                        className="tooltip tooltip-bottom"
+                        data-tip="More Detail"
+                      >
+                        <AiOutlineExclamationCircle fontSize={24} />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        )}
+      </table>
+
+      <dialog id="my_modal_2" className="modal">
+        <form
+          method="dialog"
+          className="modal-box w-1/2 max-w-5xl rounded-none"
+        >
+          <h3 className="font-bold text-lg">Broadcast Success!</h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 };
 export default SolutionTable;
